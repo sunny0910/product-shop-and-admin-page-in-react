c1197149d9fbe5aa9675ec3362c61d96b16be889
Sunny Ramrakhiani <sunnyramrakhiani10@gmail.com>
2019-05-08 13:31:38 +0530

    Added ServiceWorker to cache resource and pages and provide offline functionality

---
+++ client/public/serviceworker.js
@@ -0,0 +1,68 @@
+var cacheName = 'react-cache-v3';
+self.addEventListener('install', (event) => {
+var urlsToCache = [
+  '/serviceworker.js',
+  '/static/js/main.chunk.js',
+];
+    event.waitUntil(
+        caches.open(cacheName)
+        .then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+  self.skipWaiting();
+  console.log('Activating service worker ');
+  const whitelist = [cacheName]
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (whitelist.indexOf(cacheName) === -1) {
+            console.log('deleting :', cacheName)
+            return caches.delete(cacheName)
+          }
+        })
+        )
+    })
+  )
+})
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            // console.log('cache-hit')
+            return response;
+          }
+  
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type === 'basic' ) {
+                console.log(event.request.url, response)
+                return response;
+              }
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+              // console.log(event.request.url)
+              // console.log('cloning response')
+              caches.open(cacheName)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
+  
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
+register()
+
+function register() {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('serviceworker.js').then((registration) => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, (err)=>{
+                console.log("ServiceWorker registration failed with error: ", err);
+            });
+        });
+    }
+}
