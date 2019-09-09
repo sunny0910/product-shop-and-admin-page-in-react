39cfb09a529c184c12f0b2fc5ba5d1987471dfed
Sunny Ramrakhiani <sunnyramrakhiani10@gmail.com>
2019-09-09 16:08:05 +0530

    Commented service worker for now

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
-register()
+// register()
 
 function register() {
     if ('serviceWorker' in navigator) {
