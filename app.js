9dd3811d80ba320298c4aafd649be8621616bd0b
Sunny Ramrakhiani <sunnyramrakhiani10@gmail.com>
2019-09-10 14:41:20 +0530

    Deployment changes

--- app.js
+++ app.js
@@ -59,8 +59,8 @@ app.use(function(req, res, next) {
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')))
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + 'client' ,'build', 'index.html'))
 })
 
 // error handler
